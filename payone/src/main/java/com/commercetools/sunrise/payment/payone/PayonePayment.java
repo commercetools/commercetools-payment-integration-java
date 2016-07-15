346989f0a504d14fcbc505a090010b5c5f236b36
Laura Luiz <laura.luiz@commercetools.de>
2016-07-15 12:43:16 +0200

    Implement methods in class to avoid compile errors

--- payone/src/main/java/com/commercetools/sunrise/payment/payone/PayonePayment.java
+++ payone/src/main/java/com/commercetools/sunrise/payment/payone/PayonePayment.java
@@ -1,7 +1,25 @@
 package com.commercetools.sunrise.payment.payone;
 
 import com.commercetools.sunrise.payment.Payment;
+import io.sphere.sdk.payments.PaymentMethodInfo;
+
+import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 public class PayonePayment implements Payment {
 
+    @Override
+    public CompletionStage<List<PaymentMethodInfo>> getAvailablePaymentMethods() {
+        return null;
+    }
+
+    @Override
+    public void createPayment() {
+
+    }
+
+    @Override
+    public void getPaymentStatus() {
+
+    }
 }

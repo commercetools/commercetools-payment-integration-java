795dd03ecaebc5c74ad2f606dcd5eae5ea357470
Laura Luiz <laura.luiz@commercetools.de>
2016-07-15 12:20:48 +0200

    Define payment methods

--- common/src/main/java/com/commercetools/sunrise/payment/Payment.java
+++ common/src/main/java/com/commercetools/sunrise/payment/Payment.java
@@ -1,5 +1,14 @@
 package com.commercetools.sunrise.payment;
 
+import io.sphere.sdk.payments.PaymentMethodInfo;
+
+import java.util.List;
+
 public interface Payment { // TODO change name to something less generic
 
+    List<PaymentMethodInfo> getAvailablePaymentMethods(); // TODO decide parameters
+
+    void createPayment(); // TODO decide return type and parameters
+
+    void getPaymentStatus(); // TODO decide return type and parameters
 }

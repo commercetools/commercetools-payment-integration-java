a3bc6b8e8835262e7ea9ff727e5c622f74adb09f
Laura Luiz <laura.luiz@commercetools.de>
2016-07-15 12:32:11 +0200

    Further define payment methods

--- common/src/main/java/com/commercetools/sunrise/payment/Payment.java
+++ common/src/main/java/com/commercetools/sunrise/payment/Payment.java
@@ -3,10 +3,11 @@ package com.commercetools.sunrise.payment;
 import io.sphere.sdk.payments.PaymentMethodInfo;
 
 import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 public interface Payment { // TODO change name to something less generic
 
-    List<PaymentMethodInfo> getAvailablePaymentMethods(); // TODO decide parameters
+    CompletionStage<List<PaymentMethodInfo>> getAvailablePaymentMethods(); // TODO decide parameters
 
     void createPayment(); // TODO decide return type and parameters
 

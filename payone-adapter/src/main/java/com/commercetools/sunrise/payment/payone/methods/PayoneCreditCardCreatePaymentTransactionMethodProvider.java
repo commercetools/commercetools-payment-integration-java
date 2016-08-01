aad3ae39a4b21de387f3fe744c4ac883108b8fc9
MGA-dotsource <mga@dotsource.de>
2016-08-01 23:17:11 +0200

    #12: changed condition for 3DSecure redirect

--- payone-adapter/src/main/java/com/commercetools/sunrise/payment/payone/methods/PayoneCreditCardCreatePaymentTransactionMethodProvider.java
+++ payone-adapter/src/main/java/com/commercetools/sunrise/payment/payone/methods/PayoneCreditCardCreatePaymentTransactionMethodProvider.java
@@ -8,7 +8,6 @@ import com.commercetools.sunrise.payment.methods.CreatePaymentTransactionMethod;
 import com.commercetools.sunrise.payment.model.PaymentTransactionCreationResult;
 import io.sphere.sdk.payments.Payment;
 
-import static com.commercetools.sunrise.payment.payone.config.PayoneConfigurationNames.CREDIT_CARD_FORCE_3D_SECURE;
 import static com.commercetools.sunrise.payment.payone.config.PayoneConfigurationNames.REDIRECT_URL;
 
 /**
@@ -22,14 +21,12 @@ public class PayoneCreditCardCreatePaymentTransactionMethodProvider extends Payo
 
     @Override
     protected PaymentTransactionCreationResult handleSuccessfulServiceCall(Payment updatedPayment) {
-        if(updatedPayment.getCustom().getFieldAsBoolean(CREDIT_CARD_FORCE_3D_SECURE)) {
-            // check for a redirect URL as this leads us to 3D secure
-            String redirectURL = updatedPayment.getCustom().getFieldAsString(REDIRECT_URL);
-            if (null != redirectURL) {
-                return PaymentTransactionCreationResultBuilder.of(OperationResult.SUCCESS)
-                        .payment(updatedPayment)
-                        .handlingTask(HandlingTask.of(ShopAction.REDIRECT).redirectUrl(redirectURL)).build();
-            }
+        // check for a redirect URL as this leads us to 3D secure
+        String redirectURL = updatedPayment.getCustom().getFieldAsString(REDIRECT_URL);
+        if (null != redirectURL) {
+            return PaymentTransactionCreationResultBuilder.of(OperationResult.SUCCESS)
+                    .payment(updatedPayment)
+                    .handlingTask(HandlingTask.of(ShopAction.REDIRECT).redirectUrl(redirectURL)).build();
         }
 
         // no redirect URL

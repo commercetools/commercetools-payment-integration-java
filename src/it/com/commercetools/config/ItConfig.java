c16b7a8a2d30cfadade20b949b20d4bba8ac15f9
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2016-11-21 10:29:14 +0100

    fixed ItConfig for environment variables

--- src/it/com/commercetools/config/ItConfig.java
+++ src/it/com/commercetools/config/ItConfig.java
@@ -4,6 +4,8 @@ import io.sphere.sdk.client.SphereClientConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -13,14 +15,14 @@ import java.util.Properties;
  * <p>The class is used to access commercetools sphere client configuration values (project key, id and so on),
  * as same as other test configuration values (like {@link #getPayoneIntegrationUrl()}.
  * The values may be set in two ways:<ul>
- *     <li><i><b>it.properties</b></i> file in the resources directory (excluded from version control)</li>
+ *     <li><i><b>it.properties</b></i> file in the <i>resources/</i> directory (excluded from version control)</li>
  *     <li>via OS environment variables</li>
  * </ul>
  *
- * The properties file values have precedence over environment variables.</p>
+ * <b>The properties file values have precedence</b> over environment variables.</p>
  *
- * <p>The value keys should use prefixes: {@code ct.} and {@code CT_} respectively for properties and environment
- * variables. For instance:<br/>
+ * <p>The value keys should use prefixes: <b>{@code ct.}</b> and <b>{@code CT_}</b> respectively
+ * for properties and environment variables. For instance:<br/>
  * in properties file:
  * <pre>
  *  ct.projectKey=project-payment-test
@@ -42,8 +44,14 @@ public class ItConfig {
 
     private static final String IT_PROPERTIES_FILE = "it.properties";
 
+    /**
+     * These properties inherit behavior of {@link SphereClientConfig}, but for environment prefix we use 2 constants,
+     * because sphere concats underscore in the environment preffix, but but doesn't concat period in the properties
+     * prefix.
+     */
     private static final String PROPERTIES_PREFIX = "ct.";
-    private static final String ENVIRONMENT_PREFIX = "CT";
+    private static final String SPHERE_ENVIRONMENT_PREFIX = "CT"; // SphereClientConfig concatenates underscore itself
+    private static final String ENVIRONMENT_PREFIX = SPHERE_ENVIRONMENT_PREFIX + "_";
 
     /**
      * Try to get {@link SphereClientConfig} in the next order:<ol>
@@ -56,7 +64,7 @@ public class ItConfig {
     public static SphereClientConfig getClientConfig() {
         return IT_PROPERTIES.size() > 0
                 ? SphereClientConfig.ofProperties(IT_PROPERTIES, PROPERTIES_PREFIX)
-                : SphereClientConfig.ofEnvironmentVariables(ENVIRONMENT_PREFIX);
+                : SphereClientConfig.ofEnvironmentVariables(SPHERE_ENVIRONMENT_PREFIX);
     }
 
     /**
@@ -64,6 +72,10 @@ public class ItConfig {
      * Example:
      * <pre>http://localhost:8080/commercetools/handle/payments/</pre>
      * (if local service is running)
+     * <p>The key name is:<ul>
+     *     <li><b>{@code ct.payoneIntegrationUrl}</b> in the properties file</li>
+     *     <li><b>{@code CT_PAYONE_INTEGRATION_URL}</b> in the environment variables </li>
+     * </ul></p>
      */
     public static String getPayoneIntegrationUrl() {
         return getValueByName("payoneIntegrationUrl", "PAYONE_INTEGRATION_URL");
@@ -75,12 +87,16 @@ public class ItConfig {
     private static final Properties IT_PROPERTIES = new Properties();
 
     static {
+        // try to find and read the properties file.
+        // Since SBT (and some other tools) used execute the tests in parallel - we use static  blocking initialization
+
         try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(IT_PROPERTIES_FILE)) {
             if (in != null) {
-                LOGGER.info("Read client config from properties file \"{}\"", IT_PROPERTIES_FILE);
+                LOGGER.info("Read client config from the properties file \"{}\"", IT_PROPERTIES_FILE);
                 IT_PROPERTIES.load(in);
             } else {
-                LOGGER.info("Read client config from environment variables");
+                LOGGER.info("Read client config from the environment variables");
+                IT_PROPERTIES.clear(); // clean IT_PROPERTIES mean use environment variables.
             }
         } catch (Exception e) {
             LOGGER.warn("IT Tests properties file \"{}\" reading error: {} "
@@ -89,8 +105,8 @@ public class ItConfig {
     }
 
     /**
-     * Try to read value in the class described order:<br/>
-     *   1. Try to get from properties file
+     * Try to read value in the declared order:<br/>
+     *   1. Try to get from properties file, if exists.
      *   2. Try to get from environment variables.
      *
      * @param propName name of property (without prefix)
@@ -98,7 +114,8 @@ public class ItConfig {
      * @return value of the looking property from properties file (if the file and the property both exist)
      * or from environment variable.
      */
-    private static String getValueByName(String propName, String envName) {
+    @Nullable
+    private static String getValueByName(@Nonnull String propName, @Nonnull String envName) {
         String propKey = PROPERTIES_PREFIX + propName;
         return IT_PROPERTIES.containsKey(propKey)
                 ? IT_PROPERTIES.getProperty(propKey)

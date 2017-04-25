549ec62d0d5e7c7c9c5a3926a05ee88db14f141d
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-04-25 12:06:51 +0200

    #37: re-factor hashes calculator: move common code to base class

--- payone-adapter/src/main/java/com/commercetools/payment/payone/utils/PayoneHashCalculationProvider.java
+++ payone-adapter/src/main/java/com/commercetools/payment/payone/utils/PayoneHashCalculationProvider.java
@@ -3,9 +3,9 @@ package com.commercetools.payment.payone.utils;
 import com.commercetools.payment.payone.utils.impl.PayoneMD5HashCalculationProvider;
 import com.commercetools.payment.payone.utils.impl.PayoneSHA384HashCalculationProvider;
 
-/**
- * Created by mgatz on 8/1/16.
- */
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public interface PayoneHashCalculationProvider {
     public static PayoneHashCalculationProvider of(String hashAlgo) {
         if("MD5".equals(hashAlgo)) {
@@ -15,5 +15,25 @@ public interface PayoneHashCalculationProvider {
         return new PayoneSHA384HashCalculationProvider();
     }
 
-    public String generateHash(String data);
+    String getAlgorithm();
+
+    default String generateHash(String data) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(getAlgorithm());
+            byte[] hashBytes = md.digest(data.getBytes());
+            StringBuilder hex = new StringBuilder();
+            for (byte hashByte : hashBytes) {
+                int b = hashByte & 0xff;
+                if (Integer.toHexString(b).length() == 1) {
+                    hex.append("0");
+                }
+                hex.append(Integer.toHexString(b));
+            }
+            return hex.toString();
+        } catch (NoSuchAlgorithmException e) {
+            return data;
+        }
+    }
+
+
 }

116169e10a759255b442d8ebdab0a9c87692c222
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-04-28 14:43:20 +0200

    #39: update publish.sh script to use gradle instead of sbt

--- publish.sh
+++ publish.sh
@@ -2,5 +2,8 @@
 
 # Build, test and Publish to Maven Central (actually, target repo is depended on build.sbt settings)
 # See BUILD.md for more details about required environment to test and deploy the application
+#heroku restart --app ct-payone-integration-test && sbt clean test it:test publish-signed
 
-heroku restart --app ct-payone-integration-test && sbt clean test it:test publish-signed
+# Build, test and Publish to Bintray
+# See BUILD.md for more details about required environment to test and deploy the application
+heroku restart --app ct-payone-integration-test && ./gradlew clean build bintrayUpload

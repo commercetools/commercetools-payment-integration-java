659645d43837152f60154ef067d755946416e873
Andrii Kovalenko <andrii.kovalenko@commercetools.de>
2017-05-12 18:15:39 +0200

    #44: update payment adapter tests for default implementations

--- payone-adapter/src/test/java/com/commercetools/payment/payone/PayonePaymentServiceProviderTest.java
+++ payone-adapter/src/test/java/com/commercetools/payment/payone/PayonePaymentServiceProviderTest.java
@@ -16,6 +16,7 @@ public class PayonePaymentServiceProviderTest {
     private static final String METHOD_NAME_VOR = "BANK_TRANSFER-ADVANCE";
     private static final String METHOD_NAME_POST_EFINANCE = "BANK_TRANSFER-POSTFINANCE_EFINANCE";
     private static final String METHOD_NAME_POST_CARD = "BANK_TRANSFER-POSTFINANCE_CARD";
+    private static final String METHOD_NAME_INVOICE_KLARNA = "INVOICE-KLARNA";
     public static final String METHOD_ID = "PAYONE";
 
     @Test
@@ -26,7 +27,7 @@ public class PayonePaymentServiceProviderTest {
     @Test
     public void getAvailablePaymentMethods() throws Exception {
         PaymentServiceProvider psp = new PayonePaymentServiceProvider();
-        assertThat(psp.getAvailablePaymentMethods().size()).isEqualTo(6);
+        assertThat(psp.getAvailablePaymentMethods().size()).isEqualTo(7);
 
         PaymentMethodInfo pmi = psp.getAvailablePaymentMethods().get(0); // credit card
         assertThat(pmi.getPaymentInterface()).isEqualTo(METHOD_ID);
@@ -68,6 +69,13 @@ public class PayonePaymentServiceProviderTest {
         assertThat(pmi.getName().getLocales().size()).isEqualTo(2);
         assertThat(pmi.getName().get("en")).isEqualTo("Postfinance Card");
         assertThat(pmi.getName().get("de")).isEqualTo("Postfinance Card");
+
+        pmi = psp.getAvailablePaymentMethods().get(6); // klarna
+        assertThat(pmi.getPaymentInterface()).isEqualTo(METHOD_ID);
+        assertThat(pmi.getMethod()).isEqualTo(METHOD_NAME_INVOICE_KLARNA);
+        assertThat(pmi.getName().getLocales().size()).isEqualTo(2);
+        assertThat(pmi.getName().get("en")).isEqualTo("Pay on delivery");
+        assertThat(pmi.getName().get("de")).isEqualTo("Kauf auf Rechnung");
     }
 
     @Test
---
+++ src/test/java/com/commercetools/payment/service/PaymentServiceAdapterTest.java
@@ -0,0 +1,23 @@
+package com.commercetools.payment.service;
+
+import io.sphere.sdk.payments.PaymentMethodInfo;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PaymentServiceAdapterTest {
+
+    @Test
+    public void of_returnsDefaultPaymentAdapterServiceImpl() throws Exception {
+        assertThat(PaymentAdapterService.of()).isInstanceOf(PaymentAdapterServiceImpl.class);
+    }
+
+    @Test
+    public void getEnabledPaymentMethods_returnsAllRegisteredMethods() {
+        List<PaymentMethodInfo> enabledMethods = PaymentAdapterService.of().findAvailablePaymentMethods();
+        assertThat(enabledMethods.size()).isEqualTo(8);
+    }
+}

bf6992312920e1fad60a7150fabefb8e343e08ac
Andrii Kovalenko <andrii.kovalenko@commercetools.com>
2018-02-06 18:13:17 +0100

    #56: add tests for Payone create transaction methods calls

--- payone-adapter/src/test/java/com/commercetools/payment/payone/methods/transaction/PayonePaypalCreatePaymentTransactionMethodProviderTest.java
+++ payone-adapter/src/test/java/com/commercetools/payment/payone/methods/transaction/PayonePaypalCreatePaymentTransactionMethodProviderTest.java
@@ -1,9 +1,15 @@
 package com.commercetools.payment.payone.methods.transaction;
 
+import io.sphere.sdk.payments.PaymentMethodInfo;
+import io.sphere.sdk.payments.PaymentMethodInfoBuilder;
+import org.javamoney.moneta.Money;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.commercetools.payment.payone.config.PayonePaymentMethodKeys.WALLET_PAYPAL;
+import static io.sphere.sdk.payments.TransactionType.CHARGE;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PayonePaypalCreatePaymentTransactionMethodProviderTest
@@ -11,8 +17,18 @@ public class PayonePaypalCreatePaymentTransactionMethodProviderTest
 
     @Before
     public void setUp() throws Exception {
-        applyStubbing();
+        applyCustomFieldStubbing();
         transactionMethod = PayonePaypalCreatePaymentTransactionMethodProvider.of();
+    }
+
+    @Test
+    public void whenCreateTransactionFunctionApplied_addTransactionWithExpectedValuesIsCalled() {
+        PaymentMethodInfo paymentInterface = PaymentMethodInfoBuilder.of()
+                .paymentInterface("mockPaymentInterface")
+                .method(WALLET_PAYPAL).build();
+        applyPaymentStubbing(paymentInterface, Money.of(11.22, "ZWD"));
+
+        super.whenCreateTransactionFunctionApplied_addTransactionWithExpectedValuesIsCalled(CHARGE, Money.of(11.22, "ZWD"));
     }
 
     @Test

5aecac03eec4dd894c336811f2b65e05daefafd6
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2022-01-19 15:51:29 +0100

    fix tests.

--- src/it/com/commercetools/payment/PayoneKlarnaTest.java
+++ src/it/com/commercetools/payment/PayoneKlarnaTest.java
@@ -1,22 +1,27 @@
 package com.commercetools.payment;
 
+import com.commercetools.payment.actions.OperationResult;
 import com.commercetools.payment.domain.CreatePaymentDataBuilder;
 import com.commercetools.payment.domain.CreatePaymentTransactionDataBuilder;
 import com.commercetools.payment.payone.config.PayonePaymentMethodKeys;
 import com.commercetools.payment.model.PaymentCreationResult;
 import com.commercetools.payment.model.PaymentTransactionCreationResult;
 import com.commercetools.payment.service.PaymentAdapterService;
+import io.sphere.sdk.payments.Payment;
+import io.sphere.sdk.payments.Transaction;
 import io.sphere.sdk.types.CustomFields;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import static com.commercetools.config.ItConfig.getPayoneIntegrationUrl;
 import static com.commercetools.payment.payone.config.PayoneConfigurationNames.*;
 import static io.sphere.sdk.payments.TransactionState.FAILURE;
+import static io.sphere.sdk.payments.TransactionState.INITIAL;
 import static io.sphere.sdk.payments.TransactionType.AUTHORIZATION;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -53,7 +58,7 @@ public class PayoneKlarnaTest extends BasePayoneTest {
         assertKlarnaPaymentCreation(paymentCreationResult, reference);
 
         // user clicked "buy now" -> create transaction, trigger handle payment, return updated payment object
-        PaymentTransactionCreationResult paymentTransactionCreationResult = PaymentAdapterService.of()
+        PaymentTransactionCreationResult ptcr = PaymentAdapterService.of()
                 .createPaymentTransaction(
                         CreatePaymentTransactionDataBuilder
                                 .of(client, paymentCreationResult.getRelatedPaymentObject().get().getId())
@@ -62,7 +67,24 @@ public class PayoneKlarnaTest extends BasePayoneTest {
                 .toCompletableFuture().get();
 
         // with random data Klarna payment is expected to failure
-        assertPaymentTransactionObjectCreation(paymentTransactionCreationResult, AUTHORIZATION, FAILURE);
+
+        assertThat(ptcr).isNotNull();
+
+        assertThat(ptcr.getOperationResult())
+                .withFailMessage("Redirect URL provided from Klarna is not available.Error code: ERROR 1350, Error message: Parameter {email} faulty or missing]]");
+
+        Payment payment = ptcr.getRelatedPaymentObject().orElse(null);
+        assertThat(payment).isNotNull();
+        List<Transaction> transactions = payment.getTransactions();
+        final int size = transactions.size();
+        assertThat(size).isGreaterThan(0); // at least one has to be there
+        Transaction transaction = transactions.get(size - 1);
+        assertThat(transaction.getType()).isEqualTo(AUTHORIZATION);
+
+        // PaymentAdapterService.createPaymentTransaction() creates transaction and immediately handles the payment,
+        // thus after execution the status switches from INITIAL to some other
+        assertThat(transaction.getState()).isNotEqualByComparingTo(INITIAL);
+        assertThat(transaction.getState()).isEqualTo(FAILURE);
     }
 
     private void assertKlarnaPaymentCreation(PaymentCreationResult pcr, String reference) {

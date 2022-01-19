b4803810c606d9382567a91aed1649df28b62870
andreas Halberkamp <andreas.halberkamp@commercetools.de>
2022-01-19 14:07:31 +0100

    Adjust transaction method provider to handle redirects properly.

--- payone-adapter/src/main/java/com/commercetools/payment/payone/methods/transaction/PayoneKlarnaCreatePaymentTransactionMethodProvider.java
+++ payone-adapter/src/main/java/com/commercetools/payment/payone/methods/transaction/PayoneKlarnaCreatePaymentTransactionMethodProvider.java
@@ -1,5 +1,7 @@
 package com.commercetools.payment.payone.methods.transaction;
 
+import com.commercetools.payment.actions.OperationResult;
+import com.commercetools.payment.actions.ShopAction;
 import com.commercetools.payment.model.PaymentTransactionCreationResult;
 import io.sphere.sdk.payments.Payment;
 
@@ -11,6 +13,15 @@ public class PayoneKlarnaCreatePaymentTransactionMethodProvider extends PayoneCr
 
     @Override
     protected PaymentTransactionCreationResult handleSuccessfulServiceCall(Payment updatedPayment) {
-        return defaultSuccessTransactionCreationResult(updatedPayment);
+        return handleRedirectIfPresent(updatedPayment, this::noRedirectFallback);
+    }
+    /**
+     * For most of "redirect-based" method this action is mandatory, thus if redirect URL not found - error is returned.
+     * @param paymentWithoutRedirect updated payment reference
+     * @return {@link PaymentTransactionCreationResult} with {@link OperationResult#FAILED}
+     * and {@link ShopAction#HANDLE_ERROR}.
+     */
+    protected PaymentTransactionCreationResult noRedirectFallback(Payment paymentWithoutRedirect) {
+        return handleError("Redirect URL provided from Klarna is not available.", paymentWithoutRedirect);
     }
 }
--- payone-adapter/src/test/java/com/commercetools/payment/payone/methods/transaction/PayoneKlarnaCreatePaymentTransactionMethodProviderTest.java
+++ payone-adapter/src/test/java/com/commercetools/payment/payone/methods/transaction/PayoneKlarnaCreatePaymentTransactionMethodProviderTest.java
@@ -1,15 +1,22 @@
 package com.commercetools.payment.payone.methods.transaction;
 
+import com.commercetools.payment.actions.OperationResult;
+import com.commercetools.payment.actions.ShopAction;
+import com.commercetools.payment.model.PaymentTransactionCreationResult;
 import io.sphere.sdk.payments.PaymentMethodInfo;
 import io.sphere.sdk.payments.PaymentMethodInfoBuilder;
 import org.javamoney.moneta.Money;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static com.commercetools.payment.payone.config.PayoneConfigurationNames.REDIRECT_URL;
 import static com.commercetools.payment.payone.config.PayonePaymentMethodKeys.INVOICE_KLARNA;
 import static io.sphere.sdk.payments.TransactionType.CHARGE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PayoneKlarnaCreatePaymentTransactionMethodProviderTest
@@ -33,6 +40,20 @@ public class PayoneKlarnaCreatePaymentTransactionMethodProviderTest
 
     @Test
     public void handleSuccessfulServiceCall() throws Exception {
-        handleSuccessfulServiceCall_default();
+        String redirectUrl="www.redirectUrl.de";
+        when(customFields.getFieldAsString(Mockito.eq(REDIRECT_URL))).thenReturn(redirectUrl);
+        when(payment.getCustom()).thenReturn(customFields);
+        PaymentTransactionCreationResult creationResult = transactionMethod.handleSuccessfulServiceCall(payment);
+        assertThat(creationResult).isNotNull();
+        assertThat(creationResult.getRelatedPaymentObject()).contains(payment);
+        assertThat(creationResult.getException()).isNotPresent();
+
+        assertThat(creationResult.getOperationResult()).isEqualTo(OperationResult.SUCCESS);
+
+        assertThat(creationResult.getHandlingTask()).isNotNull();
+        assertThat(creationResult.getHandlingTask().getAction()).isEqualTo(ShopAction.REDIRECT);
+        assertThat(creationResult.getHandlingTask().getRedirectUrl().get()).isEqualTo(redirectUrl);
+
+        assertThat(creationResult.getMessage()).isNotPresent();
     }
 }
